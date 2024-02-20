95c48bf95c2e7cc72b698b84ad3c61d31d3d1d0c
Suhanova Elizaveta <suhanova.ea@edu.spbstu.ru>
2024-02-20 18:43:36 +0300

    Окончательные изменения в задании "Слова"

--- js/words.js
+++ js/words.js
@@ -1,6 +1,6 @@
 "use strict";
-
-const $ = document.querySelector.bind(document);
+//Используется сокращенная форма для получения элемента по селектору
+const $ = document.querySelector.bind(document);    
 
 const sidebar = $(".sidebar");
 const content = $(".content");
@@ -8,32 +8,42 @@ const button = $(".button");
 const textInput = $(".textinput");
 const output = $(".output");
 
-var draggableObj;
+//Переменная для хранения перетаскиваемого объекта
+var draggableObj;   
 
+//Разрешение перетаскивать объект в область контента
 content.addEventListener('dragover', function(event){
     event.preventDefault();
 })
 
+//Обработчик события drop при перетаскивании элементов в область content
 content.addEventListener('drop', function (event){
+    //Переменные для определения ближайших блоков при сбросе элемента
     var nearestLeftBlock = null;
     var nearestRightBlock = null;
     var minDistanceToLeftBlock = Infinity;
     var minDistanceToRightBlock = Infinity;
+
+    //Цикл по дочерним элементам content для определения ближайших блоков
     for(let i = 0; i < content.children.length; i++){
         var block = content.children[i];
         var rect = block.getBoundingClientRect();
         var distanceToLeftBlock = Math.abs(event.clientX - rect.right);
         var distanceToRightBlock = Math.abs(event.clientX - rect.left);
+
+        //Определение ближайшего блока слева
         if (distanceToLeftBlock >= 0 && distanceToLeftBlock < minDistanceToLeftBlock) {
             minDistanceToLeftBlock = distanceToLeftBlock;
             nearestLeftBlock = block;
         }
+        //Определение ближайшего блока справа
         if (distanceToRightBlock >= 0 && distanceToRightBlock < minDistanceToRightBlock) {
             minDistanceToRightBlock = distanceToRightBlock;
             nearestRightBlock = block;
         }
     }
 
+    //Логика вставки сбрасываемого элемента в content в зависимости от ближайших блоков
     if(minDistanceToRightBlock < minDistanceToLeftBlock && nearestLeftBlock === nearestRightBlock){
         event.target.insertBefore(draggableObj, event.target.firstChild);
     } else if (minDistanceToRightBlock > minDistanceToLeftBlock && nearestLeftBlock === nearestRightBlock){
@@ -43,38 +53,54 @@ content.addEventListener('drop', function (event){
     } else {
         event.target.insertBefore(draggableObj, nearestRightBlock);
     }
+    //Обновление содержимого output
     fillOutput();
 })
 
-button.addEventListener("click", () => {onParseText()})
+//Обработчик для нажатия на кнопку "Разобрать"
+button.addEventListener("click", () => {
+    if(textInput.value.trim() !== ""){
+        onParseText()
+    } else {
+        alert("Строка ввода не должна быть пустой!");
+    }
+})
 
-function createElement(str) {
+//Функция для создания блока с частью строки и добавления его в в левый блок
+function createBlock(str) {
     let block = document.createElement("div");
     block.setAttribute("class", "block");
     block.innerHTML = str;
     block.draggable = true;
+    //Обработчик события dragstart для сохранения перетаскиваемого объекта
     block.addEventListener('dragstart', function (event){
         draggableObj = event.target;
     })
+    //Добавление созданного блока в левое меню
     sidebar.appendChild(block);
 }
 
+//Функция для разбора введенной строки и сортировки ее частей
 function onParseText() {
     let wordsAndNumbers = textInput.value.split("-");
     var words = wordsAndNumbers.filter(item => isNaN(item)).sort();
     var numbers = wordsAndNumbers.filter(item => !isNaN(item)).sort((a, b) => a - b);
+    
+    //Все элементы строки формируются в блоки и доабвляются в левое меню
     words.forEach((word, index) => {
-        createElement("a" + (index + 1) + " " + word);
+        createBlock("a" + (index + 1) + " " + word);
     });
     numbers.forEach((number, index) => {
-        createElement("n" + (index + 1) + " " + number);
+        createBlock("n" + (index + 1) + " " + number);
     });
 }
 
+//Функция для обновления содержимого output на основе содержимого content
 function fillOutput() {
     output.innerHTML = fillOutputText();
 }
 
+//Функция для формирования выходного текста на основе содержимого из сформированных блоков в верхнем блоке
 function fillOutputText() {
     let answer = "";
     for (let i = 0; i < content.children.length; i++) {
